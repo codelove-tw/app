112f615e0afe392b65f70755ba23b63e52d84831
howtomakeaturn <howtomakeaturn@hotmail.com>
2025-09-02 20:33:05 +0800

    fix rwd

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -27,7 +27,7 @@
                 <div class="card mb-4">
                     <div class="card-body">
                         <div class="row align-items-center">
-                            <div class="col-md-6">
+                            <div class="col-md-6 mb-3 mb-md-0">
                                 <div class="btn-group" role="group">
                                     <a href="{{ route('ideas.index', ['sort' => 'latest'] + request()->query()) }}"
                                         class="btn {{ $sort === 'latest' ? 'btn-primary' : 'btn-outline-primary' }}">
